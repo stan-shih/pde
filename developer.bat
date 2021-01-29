f24a17a4b37aaabfc12ae880bfb1249e75e00b1b
Stan Shih <stan.shih@iisigroup.com>
2021-01-29 16:25:32 +0800

    Update setup environment and user information

--- developer.bat
+++ developer.bat
@@ -1,13 +1,14 @@
 @echo off
 set BASE_DIR=%~dp0
-set PATH=%BASE_DIR%\bin;%BASE_DIR%\opt\7zip;%BASE_DIR%\git;%PATH%
-if not exist "%BASE_DIR%\tmp" (
-    mkdir %BASE_DIR%\tmp
+set DEV_HOME=%BASE_DIR%
+set PATH=%DEV_HOME%\bin;%DEV_HOME%\opt\7zip;%DEV_HOME%\git;%PATH%
+if not exist "%DEV_HOME%\tmp" (
+    mkdir %DEV_HOME%\tmp
 )
-if not exist "%BASE_DIR%\git\git-bash.exe" (
-    curl -L -o %BASE_DIR%\tmp\PortableGit.7z.exe https://github.com/git-for-windows/git/releases/download/v2.22.0.windows.1/PortableGit-2.22.0-64-bit.7z.exe
-    7za x -r -o%BASE_DIR%\git %BASE_DIR%\tmp\PortableGit.7z.exe
-    %BASE_DIR%\git\git-bash.exe --no-needs-console --hide --no-cd --command=%BASE_DIR%\git\post-install.bat
+if not exist "%DEV_HOME%\git\git-bash.exe" (
+    curl -L -o %DEV_HOME%\tmp\PortableGit.7z.exe https://github.com/git-for-windows/git/releases/download/v2.22.0.windows.1/PortableGit-2.22.0-64-bit.7z.exe
+    7za x -r -o%DEV_HOME%\git %DEV_HOME%\tmp\PortableGit.7z.exe
+    %BASE_DIR%\git\git-bash.exe --no-needs-console --hide --no-cd --command=%DEV_HOME%\git\post-install.bat
 )
-cd /d %BASE_DIR%
-start %BASE_DIR%\git\git-bash.exe -c ". bin/setenv %1;bash %2 %3 %4 %5 %6 %7 %8 %9"
+cd /d %DEV_HOME%
+start %DEV_HOME%\git\git-bash.exe -c "source developer %1;bash %2 %3 %4 %5 %6 %7 %8 %9"
